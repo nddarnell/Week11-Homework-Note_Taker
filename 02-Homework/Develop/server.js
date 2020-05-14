8ab8a9e52d176f9c760c19fed4d8f597617ff6df
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-05-13 21:36:45 -0700

    app.delete in a working state now need to OOP if possible and upload to HEROKU

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -6,30 +6,31 @@ const path = require("path")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// this is required to get the layout provided to us
 app.use(express.static("public"))
 
-
+//takes you to notes page
 app.get("/notes", (req, res)=>{
     res.sendFile(__dirname + "/public/notes.html")
 })
-
+// takes you to the api with the notes
 app.get("/api/notes", (req, res)=>{
     const notes = fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data)=>{
         if (err) throw (err);
         res.json(JSON.parse(data))
     })
 })
-
+// adds a new note and gives a new id to it
 app.post("/api/notes", (req, res)=>{
     let savedNotes = fs.readFileSync(path.join(__dirname + "/db/db.json"), "utf-8")
     const parsedSavedNotes = JSON.parse(savedNotes)
+    // requesting body to then add .id to it
     const noteAdd = req.body
-    const uniqueID = function getRandomNumber(min, max){
-        min = Math.ceil(1)
-        max = Math.ceil(99999999999999)
-        return Math.floor(Math.random()*(max-min)) + min;
-    }
+    const uniqueID = Math.floor(Math.random() * 999999999)
+    //note add portion here
     noteAdd.id = uniqueID
+    // console.log(noteAdd.id)
+    //pushes the new note to api with the id
     parsedSavedNotes.push(noteAdd);
     fs.writeFileSync(path.join(__dirname + "/db/db.json"), JSON.stringify(parsedSavedNotes))
     res.json(noteAdd)
@@ -37,16 +38,19 @@ app.post("/api/notes", (req, res)=>{
 
 app.delete("/api/notes/:id", (req, res)=>{
     let savedNotes = fs.readFileSync(path.join(__dirname + "/db/db.json"), "utf-8")
+    //need to parse the notes and get or request the id back
     const parsedSavedNotes = JSON.parse(savedNotes)
     const notesId = parseInt(req.params.id)
-    
+    //basically this checks for the id to see if they match and then deletes it
     let noteAdd = parsedSavedNotes.filter((allNotes)=>{
+        // this deletes the note when id doesn match, thanks Michelle!
         return allNotes.id !== notesId
     })
+    //displays notes api after deleted note
     fs.writeFileSync(path.join(__dirname+ "/db/db.json"), JSON.stringify(noteAdd))
-    console.log("succesfully deleted note.")
+    console.log("successfully deleted note.")
 })
-
+// * takes you back to home page
 app.get("*", (req, res)=>{
     res.sendFile(__dirname + "/public/notes.html")
 })
