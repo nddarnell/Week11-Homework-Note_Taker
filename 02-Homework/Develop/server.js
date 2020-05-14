dac694623f145b1881c4fcce763ee2ee1e32c53a
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-05-13 19:21:58 -0700

    app.delete created but not fully working

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -21,17 +21,31 @@ app.get("/api/notes", (req, res)=>{
 })
 
 app.post("/api/notes", (req, res)=>{
-    const savedNotes = fs.readFileSync(path.join(__dirname + "/db/db.json"), "utf-8")
+    let savedNotes = fs.readFileSync(path.join(__dirname + "/db/db.json"), "utf-8")
     const parsedSavedNotes = JSON.parse(savedNotes)
     const noteAdd = req.body
-    const uniqueID = //math.random or date time or index number
+    const uniqueID = function getRandomNumber(min, max){
+        min = Math.ceil(1)
+        max = Math.ceil(99999999999999)
+        return Math.floor(Math.random()*(max-min)) + min;
+    }
     noteAdd.id = uniqueID
     parsedSavedNotes.push(noteAdd);
     fs.writeFileSync(path.join(__dirname + "/db/db.json"), JSON.stringify(parsedSavedNotes))
     res.json(noteAdd)
 })
 
-//app.delete needs to be done
+app.delete("/api/notes/:id", (req, res)=>{
+    let savedNotes = fs.readFileSync(path.join(__dirname + "/db/db.json"), "utf-8")
+    const parsedSavedNotes = JSON.parse(savedNotes)
+    const notesId = parseInt(req.params.id)
+    
+    let noteAdd = parsedSavedNotes.filter((allNotes)=>{
+        return allNotes.id !== notesId
+    })
+    fs.writeFileSync(path.join(__dirname+ "/db/db.json"), JSON.stringify(noteAdd))
+    console.log("succesfully deleted note.")
+})
 
 app.get("*", (req, res)=>{
     res.sendFile(__dirname + "/public/notes.html")
